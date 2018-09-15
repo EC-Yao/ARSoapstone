910a0d9b295533a90102ec7caa55d7661a38b4c4
Eddy Yao <eddy2yao2@gmail.com>
2018-09-15 11:01:22 -0400

    ARCore

--- app/src/main/java/com/example/eddyyao/arsoapstone/MainActivity.java
+++ app/src/main/java/com/example/eddyyao/arsoapstone/MainActivity.java
@@ -5,8 +5,10 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -46,5 +48,19 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         mFusedLocationClient.getLastLocation();
+    }
+
+    protected boolean getCameraPerms(){
+        return ContextCompat.checkSelfPermission(this , Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
+        if (!getCameraPerms()) {
+            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
+                    .show();
+            finish();
+        }
     }
 }
